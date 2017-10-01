e095d53224b9b42a9e4e5b1d7c74254ecfb8bfc2
harry <https://github.com/harryzhdy/gitLearning.git>
2017-10-02 02:34:32 +0800

    test2

--- src/com/zd/GitPushTest.java
+++ src/com/zd/GitPushTest.java
@@ -3,6 +3,6 @@ package com.zd;
 public class GitPushTest {
     public static void main(String[] args) {
         System.out.println("success");
-
+        System.out.println("push success");
     }
 }
